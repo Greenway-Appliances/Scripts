376592b14fae513c55f26d1d33d788ed8dd942e5
kapil <kapil.g@greenwayappliances.com>
2023-10-12 16:01:01 +0530

    connection changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // mongoURL: "mongodb://localhost:27017/sankara", //local
   // mongoURL: "mongodb://anto.raphael:antoraphael@10.0.140.199:17273/greenwayFullfilmentTest" //test development
-  mongoURL: "mongodb://anto.raphael:anto_raphael@10.0.138.69:17273/orderManagement", //prod development
+  mongoURL: "mongodb://gwCode:greenwayServerlessTest@10.0.138.69:17273/orderManagement", //prod development
   // mongoURL: "mongodb://super_admin:admin%402023@1.22.214.75:27017/Sankara-QA", //live development
   // mongoURL: "mongodb://UATAdmin:sanakarauat2023@15.207.6.229:27017/Sankara-UAT", // client dev
   // mongoURL:
